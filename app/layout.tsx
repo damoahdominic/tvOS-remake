cc34a25ad2a77afb673a3ed9c8db1d4c5a9937e5
Daniel Otoo <otoodaniel56@gmail.com>
2025-02-24 19:00:23 +0000

    settings screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from 'next/font/local'
 import "./globals.css";
+import { ViewTransitions } from 'next-view-transitions'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// Font files can be colocated inside of `pages`
+const sfPro = localFont({ src: './SF-Pro.ttf' })
 
 export const metadata: Metadata = {
   title: "Apple tvOS",
@@ -23,12 +17,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ViewTransitions>
+      <html lang="en">
+        <body
+          className={`${sfPro.className} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </ViewTransitions>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 
 import { useState, useEffect } from "react"
 import { motion } from "framer-motion"
-import { Tv, Gamepad2, ImageIcon, Music, Activity } from "lucide-react"
+import { Tv, Gamepad2, ImageIcon, Music, Activity, Video } from "lucide-react"
+import { useTransitionRouter } from "next-view-transitions"
 // import Image from "next/image"
 
 const apps = [
@@ -10,6 +11,7 @@ const apps = [
   { id: "games", icon: Gamepad2, color: "bg-pink-500", name: "Games" },
   { id: "photos", icon: ImageIcon, color: "bg-purple-500", name: "Photos" },
   { id: "music", icon: Music, color: "bg-red-500", name: "Music" },
+  { id: "video", icon: Video, color: "bg-green-500", name: "Music" },
   { id: "fitness", icon: Activity, color: "bg-black", name: "Fitness" },
 ]
 
@@ -28,6 +30,7 @@ const container = {
 export default function Home() {
   const [scrolled, setScrolled] = useState(false)
   const [focusedApp, setFocusedApp] = useState<string | null>(null)
+  const router = useTransitionRouter()
 
   useEffect(() => {
     const handleScroll = () => {
@@ -58,18 +61,18 @@ export default function Home() {
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.5 }}
         >
-          <div className="grid grid-cols-5 gap-12">
+          <div className="grid grid-cols-6 gap-9">
             {apps.map((app) => (
               <motion.button
                 key={app.id}
-                className={`relative group flex flex-col items-center`}
+                className={`relative group flex flex-col items-center group-hover:shadow-lg`}
                 onFocus={() => setFocusedApp(app.id)}
                 onBlur={() => setFocusedApp(null)}
                 whileHover={{ scale: 1.07 }}
                 whileFocus={{ scale: 1.07 }}
               >
                 <motion.div
-                  className={`aspect-video w-full rounded-2xl flex items-center justify-center ${app.color} shadow-lg`}
+                  className={`aspect-video w-full rounded-2xl flex items-center justify-center ${app.color}`}
                   animate={{
                     scale: focusedApp === app.id ? 1.07 : 1,
                   }}
@@ -91,13 +94,14 @@ export default function Home() {
 
       {/* Additional Content (for scrolling) */}
       <motion.div animate={{ bottom: scrolled ? "3rem" : "3.9rem" }} className="relative min-h-screen px-8 py-12 cursor-pointer">
-        <motion.div variants={container} initial="hidden" animate="show" className="grid grid-cols-5 gap-12 relative">
-          {Array.from({ length: 50 }).map((_, i) => (
+        <motion.div variants={container} initial="hidden" animate="show" className="grid grid-cols-6 gap-9 relative">
+          {Array.from({ length: 18 }).map((_, i) => (
             <motion.button
               key={i}
               whileHover={{ scale: 1.07 }}
               whileFocus={{ scale: 1.07 }}
-              className={`relative group flex flex-col items-center group-hover:shadow-lg group-focus:shadow-lg`}
+              onClick={() => router.push("/settings")}
+              className={`relative group flex flex-col items-center group-hover:shadow-2xl group-focus:shadow-lg`}
             // transition={{ delay: i * 0.1 }}
             >
               <motion.div
