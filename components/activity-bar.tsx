2e51e157de66a974a1711f28552aae347d2683ac
Daniel Otoo <otoodaniel56@gmail.com>
2025-02-28 16:48:05 +0000

    fixed width jump

--- components/activity-bar.tsx
+++ components/activity-bar.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { AnimatePresence, motion, MotionConfig, Transition } from "framer-motion"
+import { motion, MotionConfig, Transition } from "framer-motion"
 import { Squircle } from "@squircle-js/react"
 import moment from "moment"
 import Image from "next/image"
@@ -163,125 +163,117 @@ export default function ActivityBar() {
                     </motion.div>
                 </Squircle>
 
-                {currentTab !== "" && <Squircle
-                    asChild
-                    cornerRadius={40}
-                    cornerSmoothing={1}
-                    className="text-black/40 dark:text-white/50 backdrop-blur-[50px] cursor-pointer p-5"
-                >
-                    <motion.div className=" bg-white/50 dark:bg-[#1E1E1E]/50 transition-[width] duration-500">
-                        <AnimatePresence initial={true}>
-                            {currentTab === "profile" ?
-                                (
+                {currentTab !== "" &&
+                    <motion.div className="rounded-[40px] bg-white/50 dark:bg-[#1E1E1E]/50 transition-[width] duration-500 text-black/40 dark:text-white/50 backdrop-blur-[50px] cursor-pointer p-5">
+                        {currentTab === "profile" ?
+                            (
+                                <motion.div
+                                    variants={{ open: { opacity: 1 }, closed: { opacity: 0 } }}
+                                    initial='closed'
+                                    animate='open'
+                                    exit='closed'
+                                    className="grid grid-cols-2 gap-4">
+                                    {
+                                        Users.map((user, i) => {
+                                            return (
+                                                <motion.div
+                                                    whileHover={{ scale: 1.05 }}
+                                                    whileFocus={{ scale: 1.05 }}
+                                                    key={i}
+                                                    className="relative">
+                                                    <Image src={user.image}
+                                                        width={135}
+                                                        height={135}
+                                                        alt={user.name}
+                                                        className="rounded-full"
+                                                    />
+                                                    {user.isActive && <Image src={"/icons/checkmark.svg"}
+                                                        width={28}
+                                                        height={28}
+                                                        alt={"active"}
+                                                        className="absolute top-0 right-0"
+                                                    />}
+
+                                                    <p className="text-center">
+                                                        {user.name}
+                                                    </p>
+                                                </motion.div>
+                                            )
+                                        })
+                                    }
+
+                                    <motion.div
+                                        whileHover={{ scale: 1.05 }}
+                                        whileFocus={{ scale: 1.05 }}
+                                        className="size-[135px] bg-black/50 rounded-full flex items-center justify-center">
+                                        <Plus size={40} />
+                                    </motion.div>
+                                </motion.div>
+                            )
+                            : currentTab === "switch" ? (
+                                <motion.div
+                                    variants={{ open: { opacity: 1 }, closed: { opacity: 0 } }}
+                                    initial='closed'
+                                    animate='open'
+                                    exit='closed' className="grid grid-cols-4 grid-rows-4 gap-3">
+                                    {
+                                        Settings.map((settings, i) => {
+                                            return (
+                                                <motion.div
+                                                    whileHover={{ scale: 1.05 }}
+                                                    whileFocus={{ scale: 1.05 }}
+                                                    key={i}
+                                                    className={`rounded-[15] px-3 flex items-center gap-2 p-2 bg-black/50 ${i === 0 ?
+                                                        "col-span-2 row-span-2 aspect-square"
+                                                        : i === 1 || i === 2 || i === 3 || i === 4 ?
+                                                            "col-span-2"
+                                                            : "col-span-1"
+                                                        } ${settings.alignment === "horizontal" ? "flex-row justify-start" : "flex-col justify-center"}`}>
+                                                    <Image src={settings.icon}
+                                                        width={135}
+                                                        height={135}
+                                                        alt={settings.title}
+                                                        className={` ${i === 0 ?
+                                                            "w-[65px]"
+                                                            :
+                                                            "w-[30px]"
+                                                            }`}
+                                                    />
+                                                    {!settings.iconOnly && <p className={i > 0 ? "text-wrap w-[80px]" : ""}>
+                                                        {settings.title}
+                                                    </p>}
+                                                </motion.div>
+                                            )
+                                        })
+                                    }
+                                </motion.div>
+                            )
+                                : currentTab === "home" ? (
                                     <motion.div
                                         variants={{ open: { opacity: 1 }, closed: { opacity: 0 } }}
                                         initial='closed'
                                         animate='open'
-                                        exit='closed'
-                                        className="grid grid-cols-2 gap-4">
-                                        {
-                                            Users.map((user, i) => {
-                                                return (
-                                                    <motion.div
-                                                        whileHover={{ scale: 1.05 }}
-                                                        whileFocus={{ scale: 1.05 }}
-                                                        key={i}
-                                                        className="relative">
-                                                        <Image src={user.image}
-                                                            width={135}
-                                                            height={135}
-                                                            alt={user.name}
-                                                            className="rounded-full"
-                                                        />
-                                                        {user.isActive && <Image src={"/icons/checkmark.svg"}
-                                                            width={28}
-                                                            height={28}
-                                                            alt={"active"}
-                                                            className="absolute top-0 right-0"
-                                                        />}
+                                        exit='closed' className="">
+                                        <h1 className="text-2xl">
+                                            Home
+                                        </h1>
 
-                                                        <p className="text-center">
-                                                            {user.name}
-                                                        </p>
-                                                    </motion.div>
-                                                )
-                                            })
-                                        }
+                                        <div className="w-[250px] h-[1px] bg-black/50 dark:bg-white/50 my-2.5" />
+                                        <div className="space-y-2">
+                                            <h5 className="text-sm">SCENES</h5>
 
-                                        <motion.div
-                                            whileHover={{ scale: 1.05 }}
-                                            whileFocus={{ scale: 1.05 }}
-                                            className="size-[135px] bg-black/50 rounded-full flex items-center justify-center">
-                                            <Plus size={40} />
-                                        </motion.div>
+                                            <motion.button className="flex items-center justify-center p-3 rounded-[15] gap-2 bg-black/50">
+                                                <Image src={"/icons/moon.svg"} alt="Good Night" width={25} height={25} />
+                                                Good night
+                                            </motion.button>
+                                        </div>
                                     </motion.div>
+
                                 )
-                                : currentTab === "switch" ? (
-                                    <motion.div
-                                        variants={{ open: { opacity: 1 }, closed: { opacity: 0 } }}
-                                        initial='closed'
-                                        animate='open'
-                                        exit='closed' className="grid grid-cols-4 grid-rows-4 gap-3">
-                                        {
-                                            Settings.map((settings, i) => {
-                                                return (
-                                                    <motion.div
-                                                        whileHover={{ scale: 1.05 }}
-                                                        whileFocus={{ scale: 1.05 }}
-                                                        key={i}
-                                                        className={`rounded-[15] px-3 flex items-center gap-2 p-2 bg-black/50 ${i === 0 ?
-                                                            "col-span-2 row-span-2 aspect-square"
-                                                            : i === 1 || i === 2 || i === 3 || i === 4 ?
-                                                                "col-span-2"
-                                                                : "col-span-1"
-                                                            } ${settings.alignment === "horizontal" ? "flex-row justify-start" : "flex-col justify-center"}`}>
-                                                        <Image src={settings.icon}
-                                                            width={135}
-                                                            height={135}
-                                                            alt={settings.title}
-                                                            className={` ${i === 0 ?
-                                                                "w-[65px]"
-                                                                :
-                                                                "w-[30px]"
-                                                                }`}
-                                                        />
-                                                        {!settings.iconOnly && <p className={i > 0 ? "text-wrap w-[80px]" : ""}>
-                                                            {settings.title}
-                                                        </p>}
-                                                    </motion.div>
-                                                )
-                                            })
-                                        }
-                                    </motion.div>
-                                )
-                                    : currentTab === "home" ? (
-                                        <motion.div
-                                            variants={{ open: { opacity: 1 }, closed: { opacity: 0 } }}
-                                            initial='closed'
-                                            animate='open'
-                                            exit='closed' className="">
-                                            <h1 className="text-2xl">
-                                                Home
-                                            </h1>
-
-                                            <div className="w-[250px] h-[1px] bg-black/50 dark:bg-white/50 my-2.5" />
-                                            <div className="space-y-2">
-                                                <h5 className="text-sm">SCENES</h5>
-
-                                                <motion.button className="flex items-center justify-center p-3 rounded-[15] gap-2 bg-black/50">
-                                                    <Image src={"/icons/moon.svg"} alt="Good Night" width={25} height={25} />
-                                                    Good night
-                                                </motion.button>
-                                            </div>
-                                        </motion.div>
-
-                                    )
-                                        : currentTab === "music" &&
-                                        <MusicPlayer />
-                            }
-                        </AnimatePresence>
-                    </motion.div>
-                </Squircle>}
+                                    : currentTab === "music" &&
+                                    <MusicPlayer />
+                        }
+                    </motion.div>}
             </div>
         </MotionConfig >
     )
