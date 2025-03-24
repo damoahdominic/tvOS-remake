d49ae3990e4b430a11b04b77de8b497032b1ff18
Gilles Ashley <xylaray37@gmail.com>
2025-03-24 21:01:14 +0000

    refactor: implement grid navigation context and improve layout structure
    Added a new GridNavigationProvider for managing focused positions in the grid. Refactored layout and app item components for better readability and maintainability, including consistent import formatting and improved prop handling.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,29 @@
 import type { Metadata } from "next";
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
 import "./globals.css";
-import { ViewTransitions } from 'next-view-transitions'
+import { ViewTransitions } from "next-view-transitions";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { AppProvider } from "@/providers/app-provider";
 import { DialogProvider } from "@/providers/dialog-provider";
-import { apps } from "@/data"
+import { apps } from "@/data";
 import { AppContextMenuProvider } from "@/providers/context-menu-provider";
-
+import { GridNavigationProvider } from "@/providers/grid-navigation-provider";
 
 // Font files can be colocated inside of `pages`
-const sfPro = localFont({ src: './SF-Pro.ttf' })
+const sfPro = localFont({ src: "./SF-Pro.ttf" });
 
 export const metadata: Metadata = {
   title: "Apple tvOS",
   description: "The Best Apple tvOS Remake",
 };
 
-export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
-
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <ViewTransitions>
-        <body
-          className={`${sfPro.className} antialiased`}
-        >
+        <body className={`${sfPro.className} antialiased`}>
           <main className="relative min-h-screen overflow-hidden">
             <AppProvider appData={apps}>
               <ThemeProvider
@@ -35,7 +34,7 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
               >
                 <DialogProvider>
                   <AppContextMenuProvider>
-                    {children}
+                    <GridNavigationProvider>{children}</GridNavigationProvider>
                   </AppContextMenuProvider>
                 </DialogProvider>
               </ThemeProvider>
