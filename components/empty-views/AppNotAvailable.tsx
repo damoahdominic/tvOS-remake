a332379d0c33eb203548acf0e8722e070bd0ecff
Daniel Otoo <otoodaniel56@gmail.com>
2025-05-09 12:12:57 +0000

    app text color change

--- components/empty-views/AppNotAvailable.tsx
+++ components/empty-views/AppNotAvailable.tsx
@@ -7,8 +7,8 @@ export default function AppNotAvailable() {
     const router = useTransitionRouter()
     return (
         <div className='h-svh w-full flex items-center justify-center gap-2 flex-col bg-[#333333]'>
-            <h1 className='text-3xl font-bold'>App Not Available</h1>
-            <p className='max-w-xs mb-5'>This feature is not available to you yet.</p>
+            <h1 className='text-3xl font-bold text-white'>App Not Available</h1>
+            <p className='max-w-xs mb-5 text-white'>This feature is not available to you yet.</p>
             
             <motion.button onClick={() => router.back()} className='bg-white text-[#1E1E1E] px-3 py-2 rounded-lg w-full max-w-md'>
                 Okay
