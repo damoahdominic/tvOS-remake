2de4eff63bc05dce7b91118b040c771870acaa3a
Daniel Otoo <otoodaniel56@gmail.com>
2025-06-12 11:04:03 +0000

    added team bios

--- app/(AppsBlurBackground)/team/[id]/page.tsx
+++ app/(AppsBlurBackground)/team/[id]/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
     return (
         <div className='flex items-center justify-center h-svh overflow-hidden text-white bg-black/80'>
             {member ?
-                <div className='flex gap-10'>
+                <div className='flex gap-10 mx-auto w-3/5'>
                     <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.35, delay: 0.2 }}>
                         <motion.div style={{ backgroundColor: member.color }} className={`rounded-lg hover:scale-105 transition-all duration-300 h-full flex items-end`}>
                             <motion.div className='relative aspect-square min-w-[380px] overflow-hidden'>
@@ -31,7 +31,7 @@ export default function Page() {
                     <div className='space-y-3'>
                         <motion.h1 initial={{ opacity: 0, x: 20 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.35, delay: 0.5 }} className='text-3xl font-bold'>{member.name}</motion.h1>
                         <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.35, delay: 0.6 }} className='border border-white/50 text-white/50 rounded-md p-1 w-fit text-xs'>{member.role}</motion.p>
-                        <motion.p initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.35, delay: 0.7 }} className='w-1/2'>{member.bio}</motion.p>
+                        <motion.p initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.35, delay: 0.7 }} className=''>{member.bio}</motion.p>
 
                         <div className='space-y-5'>
                             <motion.h3 initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.35, delay: 0.8 }} className='mt-10 text-2xl font-bold'>Links</motion.h3>
