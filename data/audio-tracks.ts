b97dd87ae132e6aa9ba397a55d7bb88b4b2d9461
Daniel Otoo <otoodaniel56@gmail.com>
2025-06-16 19:13:27 +0000

    jingles

--- data/audio-tracks.ts
+++ data/audio-tracks.ts
@@ -4,42 +4,60 @@ import { Track } from '@/providers/audio-provider';
 export const defaultTracks: Track[] = [
     {
         id: 1,
-        title: "Grind day",
-        artist: "Kwesi Arthur",
-        duration: 251, // in seconds
-        albumArt: "https://res.cloudinary.com/dgfdign4l/image/upload/v1744181963/Grind_Day_-_Kwesi_Arthur_ym3lm5.png",
-        audioSrc: "https://res.cloudinary.com/dgfdign4l/video/upload/v1744136830/Grind_Day_-_Kwesi_Arthur_ntr8n0.mp3"
+        title: "Jingle One",
+        artist: "Dom&Larry",
+        duration: 13, // in seconds
+        albumArt: "https://res.cloudinary.com/dg41c7v3d/image/upload/v1750100396/Album_Art_cwoiwx.svg",
+        audioSrc: "https://res.cloudinary.com/dg41c7v3d/video/upload/v1750099545/ThemeSong1_hceoqr.mp3"
     },
     {
         id: 2,
-        title: "Original",
-        artist: "Sarkodie",
-        duration: 274,
-        albumArt: "https://res.cloudinary.com/dgfdign4l/image/upload/v1744181963/Original_Sarkodie_rlrtko.png",
-        audioSrc: "https://res.cloudinary.com/dgfdign4l/video/upload/v1744136830/Original_-_Sarkodie_slh4pz.mp3"
+        title: "Jingle Two",
+        artist: "Dom&Larry",
+        duration: 15,
+        albumArt: "https://res.cloudinary.com/dg41c7v3d/image/upload/v1750100450/Album_Art_qbzrcm.svg",
+        audioSrc: "https://res.cloudinary.com/dg41c7v3d/video/upload/v1750099581/ThemeSong2_oomyxz.wav"
     },
-    {
-        id: 3,
-        title: "Anthem",
-        artist: "Kwesi Arthur",
-        duration: 192,
-        albumArt: "https://res.cloudinary.com/dgfdign4l/image/upload/v1744181963/Anthem_-_Kwesi_Arthur_bhbhbk.png",
-        audioSrc: "https://res.cloudinary.com/dgfdign4l/video/upload/v1744136829/Anthem_-_Kwesi_Arthur_gcrqv6.mp3"
-    },
-    {
-        id: 4,
-        title: "Placebo",
-        artist: "La meme gang",
-        duration: 311,
-        albumArt: "https://res.cloudinary.com/dgfdign4l/image/upload/v1744181963/Placebo_-_Darkovibes_vmd76f.png",
-        audioSrc: "https://res.cloudinary.com/dgfdign4l/video/upload/v1744136830/Placebo_-_DarkoVibes_rheahi.mp3"
-    },
-    {
-        id: 5,
-        title: "Pray for me",
-        artist: "Kwesi Arthur",
-        duration: 305,
-        albumArt: "https://res.cloudinary.com/dgfdign4l/image/upload/v1744181963/Pray_For_Me_Kwesi_Arthur_aqdvmm.png",
-        audioSrc: "https://res.cloudinary.com/dgfdign4l/video/upload/v1744136830/Pray_For_Me_-_Kwesi_Arthur_nr9vl9.mp3"
-    }
 ];
+// export const defaultTracks: Track[] = [
+//     {
+//         id: 1,
+//         title: "Grind day",
+//         artist: "Kwesi Arthur",
+//         duration: 251, // in seconds
+//         albumArt: "https://res.cloudinary.com/dgfdign4l/image/upload/v1744181963/Grind_Day_-_Kwesi_Arthur_ym3lm5.png",
+//         audioSrc: "https://res.cloudinary.com/dgfdign4l/video/upload/v1744136830/Grind_Day_-_Kwesi_Arthur_ntr8n0.mp3"
+//     },
+//     {
+//         id: 2,
+//         title: "Original",
+//         artist: "Sarkodie",
+//         duration: 274,
+//         albumArt: "https://res.cloudinary.com/dgfdign4l/image/upload/v1744181963/Original_Sarkodie_rlrtko.png",
+//         audioSrc: "https://res.cloudinary.com/dgfdign4l/video/upload/v1744136830/Original_-_Sarkodie_slh4pz.mp3"
+//     },
+//     {
+//         id: 3,
+//         title: "Anthem",
+//         artist: "Kwesi Arthur",
+//         duration: 192,
+//         albumArt: "https://res.cloudinary.com/dgfdign4l/image/upload/v1744181963/Anthem_-_Kwesi_Arthur_bhbhbk.png",
+//         audioSrc: "https://res.cloudinary.com/dgfdign4l/video/upload/v1744136829/Anthem_-_Kwesi_Arthur_gcrqv6.mp3"
+//     },
+//     {
+//         id: 4,
+//         title: "Placebo",
+//         artist: "La meme gang",
+//         duration: 311,
+//         albumArt: "https://res.cloudinary.com/dgfdign4l/image/upload/v1744181963/Placebo_-_Darkovibes_vmd76f.png",
+//         audioSrc: "https://res.cloudinary.com/dgfdign4l/video/upload/v1744136830/Placebo_-_DarkoVibes_rheahi.mp3"
+//     },
+//     {
+//         id: 5,
+//         title: "Pray for me",
+//         artist: "Kwesi Arthur",
+//         duration: 305,
+//         albumArt: "https://res.cloudinary.com/dgfdign4l/image/upload/v1744181963/Pray_For_Me_Kwesi_Arthur_aqdvmm.png",
+//         audioSrc: "https://res.cloudinary.com/dgfdign4l/video/upload/v1744136830/Pray_For_Me_-_Kwesi_Arthur_nr9vl9.mp3"
+//     }
+// ];
